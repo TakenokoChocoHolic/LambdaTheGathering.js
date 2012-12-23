32932e86677d1cafd623a8812c001ba3f5e6b866
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2012-12-23 18:29:42 +0900

    Add step function

--- js/game.js
+++ js/game.js
@@ -5,31 +5,39 @@ var Game = {};
     root.depth = 0;
 
     var zero = function () {
+        root.depth++;
         return 0;
     };
 
     var succ = function (n) {
+        root.depth++;
         return n + 1;
     };
 
     var dbl = function (n) {
+        root.depth++;
         return n * 2;
     };
 
     var get = function (i) {
+        root.depth++;
         return i;
     };
 
     var put = function () {
+        root.depth++;
         return I;
     };
 
     var I = function (x) {
+        root.depth++;
         return x;
     };
 
     var K = function (x, y) {
+        root.depth++;
         var KK = function () {
+            root.depth++;
             return x;
         };
         if (typeof y === "undefined") {
@@ -40,15 +48,18 @@ var Game = {};
     };
 
     var S = function (f, g, x) {
+        root.depth++;
         if (typeof g === "undefined") {
             return function (gg, xx) {
                 var ff = f;
+                root.depth++;
 
                 if (typeof xx === "undefined") {
                     return function (xxx) {
                         var fff = ff,
                             ggg = gg;
 
+                        root.depth++;
                         return fff(xxx)(ggg(xxx));
                     };
                 } else {
@@ -60,6 +71,7 @@ var Game = {};
                 var ff = f,
                     gg = g;
 
+                root.depth++;
                 return ff(xx)(gg(xx));
             };
         } else {
@@ -68,10 +80,12 @@ var Game = {};
     };
 
     var inc = function (i) {
+        root.depth++;
         return i;
     };
 
     var dec = function (i) {
+        root.depth++;
         return i;
     };
 
@@ -82,6 +96,7 @@ var Game = {};
                 var iii = ii,
                     jjj = jj;
 
+                root.depth++;
                 // TODO: error check
                 var proponent = root.state.player[root.state.turn%2];
                 var opponent = root.state.player[1-root.state.turn%2];
@@ -89,18 +104,36 @@ var Game = {};
                 opponent.slot[255-jjj].vitality -= ~~(nnn * 9 / 10);
             };
 
+            root.depth++;
             if (typeof nn === "undefined") {
                 return __attack;
             } else {
                 return __attack(nn);
             }
         };
+
+        root.depth++;
         if (typeof j === "undefined") {
             return _attack;
         } else if (typeof n === "undefined") {
             return _attack(j);
         } else {
             return _attack(j, n);
+        }
+    };
+
+    var step = function (slot_num, func, dir, state) {
+        root.state = state;
+        root.depth = 0;
+
+        var t = state.turn % 2,
+            proponent = state.player[t],
+            slot = proponent.slot[slot_num];
+
+        if (true) {
+            slot.value = slot.value(func);
+        } else {
+            slot.value = func(slot.value);
         }
     };
 
@@ -115,6 +148,7 @@ var Game = {};
     root.inc = inc;
     root.dec = dec;
     root.attack = attack;
+    root.step = step;
 
 })(Game);
 
