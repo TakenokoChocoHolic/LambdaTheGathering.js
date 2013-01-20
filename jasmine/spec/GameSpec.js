7070fe9dadd433c887399b5aacf429eaaa72a69c
Kazunori Sakamoto <exkazuu@gmail.com>
2013-01-20 17:07:11 +0900

    Add a test case for I.

--- jasmine/spec/GameSpec.js
+++ jasmine/spec/GameSpec.js
@@ -1,7 +1,6 @@
 var Game;
 
 describe("Example", function () {
-
     it("1 should be true", function () {
         expect(1).toBeTruthy();
     });
@@ -9,14 +8,11 @@ describe("Example", function () {
 });
 
 describe("Game", function () {
-
     beforeEach(function () {
         Game.depth = 0;
     });
 
     describe("#depth", function () {
-        
-
         it("is 0 when did nothing", function () {
             expect(Game.depth).toEqual(0);
         });
@@ -25,11 +21,9 @@ describe("Game", function () {
             Game.zero();
             expect(Game.depth).toEqual(1);
         });
-
     });
 
     describe("#zero", function () {
-
         beforeEach(function () {
             
         });
@@ -37,15 +31,17 @@ describe("Game", function () {
         it("should be equal to 0", function () {
             expect(Game.zero()).toEqual(0);
         });
-
     });
 
     describe("succ(succ(zero))", function () {
-
         it("should be equal to 2", function () {
             expect(Game.succ(Game.succ(Game.zero()))).toEqual(2);
         });
-
     });
 
+    describe("I(zero)", function () {
+        it("should be equal to 0", function () {
+            expect(Game.cI(Game.zero)).toEqual(0);
+        });
+    });
 });
