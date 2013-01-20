d67ac69918afe9ae3bca88e6ac690673be5a88e6
Kazunori Sakamoto <exkazuu@gmail.com>
2013-01-20 17:29:18 +0900

    Add test cases with test scenario in official site.

--- jasmine/spec/GameSpec.js
+++ jasmine/spec/GameSpec.js
@@ -45,3 +45,23 @@ describe("Game", function () {
         });
     });
 });
+
+describe("Game with state", function() {
+    var state;
+
+    beforeEach(function () {
+        Game.depth = 0;
+        state = Game.initState();
+    });
+
+    describe("when executing the 1st turn of the 1st exmaple actions in the official site", function() {
+        it("should have correct slots", function () {
+            Game.step(0, Game.zero, false, state);
+            Game.step(0, Game.inc, false, state);
+            exepct(state.player[0].slot[0].value).toEqual(0);
+            exepct(state.player[0].slot[0].vitality).toEqual(10000);
+            exepct(state.player[1].slot[0].value).toEqual(Game.inc);
+            exepct(state.player[1].slot[0].vitality).toEqual(10000);
+        });
+    });
+});
